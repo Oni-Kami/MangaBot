6ecdb5ef32b99aaedb5c5a91240c89099a56abf9
Oni-Kami <huzaifaalternate@gmail.com>
2025-02-24 14:49:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 env_vars = {
   # Get From my.telegram.org
-  "API_HASH": "",
+  "API_HASH": "f1ff78d3d0b3eaa825294af918318ac7",
   # Get From my.telegram.org
-  "API_ID": "",
+  "API_ID": "21531216",
   #Get For @BotFather
-  "BOT_TOKEN": "",
+  "BOT_TOKEN": "7182062183:AAEhFXSxp414Nqv4S1Y1WtHjVZgEre-nss8",
   # Get For tembo.io
-  "DATABASE_URL_PRIMARY": "",
+  "DATABASE_URL_PRIMARY": "postgresql://postgres:.....@justly-mighty-genet.data-1.use1.tembo.io:5432/postgres",
   # Logs Channel Username Without @
-  "CACHE_CHANNEL": "",
+  "CACHE_CHANNEL": "-1002198221587",
   # Force Subs Channel username without @
-  "CHANNEL": "",
+  "CHANNEL": "Manga_Blast",
   # {chap_num}: Chapter Number
   # {chap_name} : Manga Name
   # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
-  "FNAME": "",
+  "FNAME": "Chapter {chap_num} {chap_name} @Manga_Blast",
   # Put Thumb Link 
   "THUMB": ""
 }
